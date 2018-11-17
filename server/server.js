bb7fe121e0629d4cb962857d6c5a90ba7948620f
Panu Kettunen <panu.t.kettunen@gmail.com>
2018-11-17 18:19:28 +0200

    fix: modified path for getting client files

--- server/server.js
+++ server/server.js
@@ -25,10 +25,10 @@ export default function startServer(store) {
 
   if (process.env.NODE_ENV === "production") {
     // Serve any static files
-    app.use(express.static(path.join(__dirname, "client/build")));
+    app.use(express.static("../client/build"));
     // Handle React routing, return all requests to React app
     app.get("*", function(req, res) {
-      res.sendFile(path.join(__dirname, "client/build", "index.html"));
+      res.sendFile("../client/build", "index.html");
     });
   }
 
